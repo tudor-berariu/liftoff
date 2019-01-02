d9f88561f5fe7f1da74a9643e8fe748c0f79724e
Tudor BERARIU <tberariu@bitdefender.com>
2019-01-02 19:35:28 +0200

    Added some docstrings for liftoff_config

--- liftoff/common/liftoff_config.py
+++ liftoff/common/liftoff_config.py
@@ -1,9 +1,16 @@
 import os
 import os.path
+from typing import List, Optional
 import yaml
 
 
 def update_configs(original, diff):
+    """ This function takes updates the first dictionary using values from the
+        second. It an entry in the second dictionary has a key starting with
+        '_' then it replaces the full value (removing the '_') instead of going
+        recurisively in depth.
+    """
+
     for key, value in diff.items():
         if key.startswith("_"):
             original[key[1:]] = value
@@ -16,6 +23,12 @@ def update_configs(original, diff):
 
 
 def get_liftoff_config(only_local: bool = False) -> dict:
+    """ This function returns a dictionary with all the settings for liftoff.
+        It first loads the global config from ~/.liftoff_cfg.yaml (if exists).
+        If there is a local (project) configuration it is used to update the
+        global configuration.
+    """
+
     local_cfg, global_cfg = None, None
 
     if os.path.isfile(".liftoff_cfg.yaml"):
@@ -33,6 +46,21 @@ def get_liftoff_config(only_local: bool = False) -> dict:
 
     update_configs(global_cfg, local_cfg)
     return global_cfg
+
+
+def get_liftoff_option(keys: List[str]) -> Optional[object]:
+    """ This function reads some option from the config file. It should be used
+        only when one is not interested in more than one setting. In that case
+        call 'get_liftoff_config' and get all the values you need form that
+        dictionary.
+    """
+
+    liftoff_cfg = get_liftoff_config()
+    for key in keys:
+        if not isinstance(liftoff_cfg, dict) or key not in liftoff_cfg:
+            return None
+        liftoff_cfg = liftoff_cfg[key]
+    return liftoff_cfg
 
 
 def save_local_options(update_cfg: dict):
