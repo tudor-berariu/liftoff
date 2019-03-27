361bd7da9a1da6036a0ec0e8e7a30cc1ed6629da
Tudor BERARIU <tberariu@bitdefender.com>
2019-03-27 14:19:40 +0200

    Remove old liftoff

--- oldliftoff/common/miscellaneous.py
+++
@@ -1,7 +0,0 @@
-def ask_user_yn(question: str) -> bool:
-    while True:
-        answer = str(input(f"\n{question:s} (y/n): ")).lower().strip()
-        if answer == "y":
-            return True
-        if answer == "n":
-            return False
