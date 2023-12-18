006e82993f6b37b60495e75d179bbc9dc82a19bf
mariusdgm <dragomir10m@yahoo.com>
2023-12-18 10:13:08 +0200

    updated minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = "0.4.0"  # single source of truth
+VERSION = "0.4.1"  # single source of truth
 print("-- Installing liftoff " + VERSION)
 with open("./liftoff/version.py", "w") as f:
     f.write("__version__ = '{}'\n".format(VERSION))
